e9540f74a909b7647483e409c4e16d49f321b6b6
hoon <dohun10170@gmail.com>
2022-07-13 19:27:28 +0900

    순수 JPA기반 리포지토리 만들기

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -9,10 +9,13 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class MemberJpaRepositoryTest {
 
     @Autowired
@@ -22,7 +25,6 @@ class MemberJpaRepositoryTest {
     EntityManager em;
 
     @Test
-    @Transactional
     public void testMember() throws Exception {
         //given
         Member member = new Member("memberA");
@@ -36,5 +38,37 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertEquals(member, findMember);
+    }
+
+    @Test
+    public void basicCRUD() throws Exception {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //when - then
+
+        //단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertEquals(member1, findMember1);
+        assertEquals(member2, findMember2);
+
+        //리스트 조회 검증
+        List<Member> members = memberJpaRepository.findAll();
+        assertEquals(members.size(), 2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertEquals(count, 2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        long deletedCount = memberJpaRepository.count();
+        assertEquals(deletedCount, 0);
+
     }
 }
